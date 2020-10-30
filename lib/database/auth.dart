0e305c2225a84f13bf0a82ad3b9d3014304d5ffd
yashmehta <yash181999@gmail.com>
2020-10-31 03:59:48 +0530

    With Authentication Login Signup

--- lib/database/auth.dart
+++ lib/database/auth.dart
@@ -1,7 +1,8 @@
-import 'package:bhrammanbeta/database/firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'firestore.dart';
 
 class AuthService {
   FirebaseAuth _auth  = FirebaseAuth.instance;
