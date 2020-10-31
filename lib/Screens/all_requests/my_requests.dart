3787451213d5a26b1900a60ee423c4e598d68dc0
yashmehta <yash181999@gmail.com>
2020-10-31 22:09:47 +0530

    Request Backend Completed

--- lib/Screens/all_requests/my_requests.dart
+++ lib/Screens/all_requests/my_requests.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:versionbeta3/color/color.dart';
+import 'package:versionbeta3/database/auth.dart';
 
 
 class MyRequests extends StatefulWidget {
@@ -8,102 +10,139 @@ class MyRequests extends StatefulWidget {
 }
 
 class _MyRequestsState extends State<MyRequests> {
+
+
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getUserId();
+  }
+  String userId = "";
+  getUserId() {
+    AuthService.getUserIdSharedPref().then((value) {
+      setState(() {
+        userId = value;
+      });
+    });
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          height: 150,
-          child: Card(
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        padding:EdgeInsets.all(3) ,
-                        decoration: BoxDecoration(
-                          color: Colors.redAccent,
-                        ),
-                        child: Text(
-                            "A+",
-                             style: TextStyle(
-                               color: white,
-                             ),
-                        ),
-                      ),
-                      SizedBox(width: 5,),
+        body: StreamBuilder(
+            stream: Firestore.instance.collection("Requests").where("sendByUserId", isEqualTo: userId).snapshots(),
+            builder: (context, snapshot) {
+              return snapshot.hasData ?  ListView.builder(
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot doc = snapshot.data.documents[index];
+                    return Container(
+                      height: 150,
+                      child: Card(
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          child: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    padding:EdgeInsets.all(3) ,
+                                    decoration: BoxDecoration(
+                                      color: Colors.redAccent,
+                                    ),
+                                    child: Text(
+                                      doc['bloodGroup'],
+                                      style: TextStyle(
+                                        color: white,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(width: 5,),
 
-                      Container(
-                        child: Text(
-                            "Yash Mehta",
-                            style: TextStyle(
-                              fontFamily: 'sf_pro_bold',
-                              fontSize: 18
-                            ),
-                        ),
-                      ),
+                                  Container(
+                                    child: Text(
+                                      doc['name'],
+                                      style: TextStyle(
+                                          fontFamily: 'sf_pro_bold',
+                                          fontSize: 18
+                                      ),
+                                    ),
+                                  ),
 
-                    ],
-                  ),
+                                ],
+                              ),
 
-                  SizedBox(height: 8,),
+                              SizedBox(height: 8,),
 
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("Date"),
-                      Text("Time"),
-                      Text("Location")
-                    ],
-                  ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text("Date"),
+                                  Text("Time"),
+                                  Text("Location")
+                                ],
+                              ),
 
-                  SizedBox(height: 4,),
+                              SizedBox(height: 4,),
 
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
 
-                    children: [
-                      Text("4/6/2020"),
-                      Text("14:35"),
-                      Text("Nagpur"),
-                    ],
+                                children: [
+                                  Text(doc['date']),
+                                  Text(doc['time']),
+                                  Text(doc['location']),
+                                ],
 
-                  ),
+                              ),
 
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      RaisedButton(
-                        color: Colors.green,
-                        onPressed: (){},
-                        child: Text(
-                            "Accept Request",
-                             style: TextStyle(
-                               color: white,
-                             ),
-                        ),
-                      ),
+                              SizedBox(height: 20,),
 
-                      RaisedButton(
-                        onPressed: (){},
-                        child: Text(
-                          "Reject Request",
-                          style: TextStyle(
-                            color: Colors.black,
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+
+
+
+                                  Text(
+                                      "Status : ",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+
+                                  Text(
+                                    doc['status'],
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      color: red,
+                                    ),
+                                  ),
+
+
+
+                                ],
+                              ),
+
+
+                            ],
                           ),
                         ),
                       ),
-
-                    ],
-                  ),
-
-
-                ],
-              ),
-            ),
-          ),
-        ),
+                    );
+                  }
+              ) :Center(
+                child: Container(
+                  child: Text("NO Requests FOUND"),
+                ),
+              );
+            }
+        )
     );
   }
 }
